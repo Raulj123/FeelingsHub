6c518746b86cb3a705813f082447158e2025aacc
IamRaul <jarquinr121@gmail.com>
2023-04-27 21:16:12 -0700

    first kinda

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+import '@picocss/pico'
+
+</script>
+<h1 style="text-align:center; margin-top:20px;">BeReal but no photos</h1>
+  <slot/>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,34 @@
+
+<form method="POST">
+  <div class="grid">
+    <label for="firstname">
+      First name
+      <input type="text" id="first" placeholder="First name" required>
+    </label>
+      <label for ="content">
+      How did you feel today?
+      <input type="text" id="feel" placeholder="shit" required>
+    </label> 
+  </div>
+    <button type="submit">Submit</button>
+</form>
+
+
+
+<style>
+  .grid{
+    width:595px;
+    text-align:center;
+    justify-items:center;
+    display:flex;
+    margin-left:auto;
+    margin-right:auto;
+    padding:10px;
+    margin-top:15x;
+  }
+  button{
+    width:40%;
+    margin-left:auto;
+    margin-right:auto;
+  }
+</style>
